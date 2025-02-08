9d2f349020acc60576b461f6897ae23aa868b4cb
Arturo <117057045+artuvall@users.noreply.github.com>
2025-02-08 14:05:10 -0500

    exercise 06 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400 === 0) {
+        return true; //divisible by 400, leap years
+    } else if (year % 100 === 0) {
+        return false; //divisible by 100 but not 400, not leap years
+    } else if (year % 4 === 0) {
+        return true; //divisible by 4, leap years
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
